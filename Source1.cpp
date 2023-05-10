f64a0ded9c78d0700ae0470e52c39dedd4689363
Nazar <nazariximail@gmail.com>
2023-05-10 11:48:39 +0300

    New code

--- Source1.cpp
+++ Source1.cpp
@@ -2,7 +2,29 @@
 
 using namespace std;
 
+void IntArray(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Enter array [" << i << "]", cin >> arr[i];
+	}
+}
+void PrintArray(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
 int main()
 {
+	int size;
+	cout << "Enter size: ", cin >> size;
+
+	int* arr = new int[size];
+	IntArray(arr, size);
+	PrintArray(arr, size);
 	return 0;
 }
+
